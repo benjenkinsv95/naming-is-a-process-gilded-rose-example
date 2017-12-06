63a56df0d7cbdee086ba08e02e28014d33e25c4a
Ben Jenkins <benjenkinsv95@gmail.com>
2017-12-06 11:08:39 -0500

    updateExpiredItemQuality insight

--- step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
+++ step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
@@ -44,20 +44,24 @@ class GildedRose {
         updateSellIn(item);
 
         if (itemExpired(item)) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+            updateExpiredItemQuality(item);
+        }
+    }
+
+    private void updateExpiredItemQuality(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = item.quality - item.quality;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = item.quality - item.quality;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
