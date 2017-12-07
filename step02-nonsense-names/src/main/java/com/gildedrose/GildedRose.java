182de5fa14387cab57111a5ec50546df1c53c566
ben <benjenkinsv95@gmail.com>
2017-12-07 10:28:11 -0500

    updateExpiredItemQuality insight

--- step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
+++ step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
@@ -21,22 +21,26 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                        updateBackstagePassQuality(items[i]);
                     }
                 }
             }
 
             updateBasedOnSellIn(items[i]);
+        }
+    }
+
+    private void updateBackstagePassQuality(Item item) {
+        if (item.sellIn < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        if (item.sellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
         }
     }
 
