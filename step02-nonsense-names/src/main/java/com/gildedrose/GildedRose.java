09126cfa325813d3c3d299e36094b08208865f5f
ben <benjenkinsv95@gmail.com>
2017-12-07 10:34:43 -0500

    item name insights

--- step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
+++ step02-nonsense-names/src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,9 @@ package com.gildedrose;
 import java.util.Arrays;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -15,10 +18,10 @@ class GildedRose {
     }
 
     private void updateItemQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!item.name.equals(AGED_BRIE)
+                && !item.name.equals(BACKSTAGE_PASSES)) {
             if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!item.name.equals(SULFURAS)) {
                     item.quality = item.quality - 1;
                 }
             }
@@ -26,7 +29,7 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.name.equals(BACKSTAGE_PASSES)) {
                     updateBackstagePassQuality(item);
                 }
             }
@@ -58,10 +61,10 @@ class GildedRose {
     }
 
     private void updateExpiredItemQuality(Item item) {
-        if (!item.name.equals("Aged Brie")) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!item.name.equals(AGED_BRIE)) {
+            if (!item.name.equals(BACKSTAGE_PASSES)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -80,7 +83,7 @@ class GildedRose {
     }
 
     private void updateSellIn(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (!item.name.equals(SULFURAS)) {
             item.sellIn = item.sellIn - 1;
         }
     }
